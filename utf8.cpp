e8624d585bd6cc06ad1c62e3bf58386d055771a6
Pu-Feng Du <pufengdu@gmail.com>
2022-12-13 22:20:02 +0800

    Update utf8.cpp

--- utf8.cpp
+++ utf8.cpp
@@ -10,7 +10,7 @@ using namespace std;
 // Settings -> Editor -> Encoding Settings -> Use this encoding = As default encoding (bypass...)
 // Settings -> Editor -> Encoding Settings -> If conversion fails using this settings above, try... = [tick]
 // Registry:
-// HKEY_CURRENT_USER\Console\D:_Work_bin_codeblocks_cb_console_runner.exe
+// HKEY_CURRENT_USER\Console\$cb_code_runner full path$
 // \CodePage = 65001
 /* The following is a template of Registry file, replace thost $XXX$
 Windows Registry Editor Version 5.00
